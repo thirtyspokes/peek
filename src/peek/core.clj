2716c359ef2eb753e4c695cc242ec55000e52605
Ray Ashman Jr <ray@rayashmanjr.com>
2016-10-31 18:52:41 -0400

    Improve time!

--- src/peek/core.clj
+++ src/peek/core.clj
@@ -2,6 +2,20 @@
   (:require [clojure.core.async :refer [go]]
             [peek.datadog :as d]))
 
+(defn gauge!
+  "Records the current `value` of the gauge identified by `key`.  Executes
+   asynchronously and immediately returns `nil`.
+
+   Optinally accepts an options map with keys:
+
+   :tags - A map of tags to append to this measurement.
+   :sample - A rate between (0.0 and 1.0) at which to sample this gauge.
+
+   Defaults:
+   :tags {}, :sample 1.0"
+  [key value & {:keys [tags sample] :or {tags {} sample 1.0}}]
+  nil)
+
 (defn timing!
   "Records a timing of `value` milliseconds for the metric identified
    by `key`.  The body is executed in a go block so this function will
@@ -10,7 +24,7 @@
    Optionally accepts an options map with keys:
 
    :tags - A map of tags to append to this measurement.
-   :sample - A rate between (0.0 and 1.0) and which to sample this timing.
+   :sample - A rate between (0.0 and 1.0) at which to sample this timing.
 
    Defaults:
    :tags {}, :sample 1.0"
@@ -19,8 +33,8 @@
   nil)
 
 (defn increment!
-  "Increments the counter specified by `key`.  The body is executed
-   in a go block so this function will immediately return `nil`.
+  "Increments the counter specified by `key`.  Executes 
+   asynchronously and immediately returns `nil`.
 
    Optionally accepts an options map with keys:
    
@@ -34,16 +48,35 @@
   (go (d/submit-counter key delta tags sample))
   nil)
 
+(defn- withtime
+  "Allows multi-arity usage of `time!`."
+  ([key form]
+   (withtime key {} 1.0 form))
+  ([key tags form]
+   (withtime key tags 1.0 form))
+  ([key tags sample-rate form]
+   `(let [start# (System/currentTimeMillis)
+          res# (eval (quote ~form))]
+      (d/submit-timing
+        (quote ~key)
+        (- (System/currentTimeMillis) start#)
+        (quote ~tags)
+        (quote ~sample-rate))
+      res#)))
+
 (defmacro time!
-  "Wraps `form` in a Datadog timing - starts the timer, executes the code in 
+  "Wraps `form` in a Datadog timing - starts the timer, evaluates the code in 
    `form`, submits the execution time as a timing packet, and then returns the
-  result of `form`."
-  [key tags sample-rate form]
-  `(let [start# (System/currentTimeMillis)
-         res# (eval (quote ~form))]
-     (d/submit-timing
-       (quote ~key)
-       (- (System/currentTimeMillis) start#)
-       (quote ~tags)
-       (quote ~sample-rate))
-     res#))
+  result of evaluating `form`.
+
+  `time!` requires at minimum a `key` for the timing to be recorded:
+
+  (time! \"login.successful\" ( ... your code to be instrumented ... )
+
+  In addition to the `key`, you may also optionally provide `tags` and a `sample-rate`
+  as in the other metric functions, but they must be in that order, e.g. if you wish
+  to not supply tags but use a non-default sample rate:
+
+  (time! \"login.successful\" {} 0.25 ( ... your code to be instrumented ... )"
+  [& args]
+  (apply withtime args))
